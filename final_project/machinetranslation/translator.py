4dd1d1bee75ccdd45e58daa9d59f3b94d620f8c7
DerSimulant <82386236+DerSimulant@users.noreply.github.com>
2022-12-31 08:36:16 +0100

    Update translator.py

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -8,3 +8,18 @@ load_dotenv()
 
 apikey = os.environ['apikey']
 url = os.environ['url']
+
+authenticator = IAMAuthenticator('apikey')
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01'
+    authenticator=authenticator
+)
+
+language_translator.set_service_url('url')
+
+translation = language_translator.translate(
+    text='Hello, how are you today?',
+    model_id='en-es').get_result()
+print(json.dumps(translation, indent=2, ensure_ascii=False))
+
+#bisher Versionsfehler
