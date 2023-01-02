e5c0c17fa2c391eb996c2308125e6af776e25f93
DerSimulant <82386236+DerSimulant@users.noreply.github.com>
2023-01-02 16:49:59 +0100

    Update server.py
    added functions for endpoints

--- final_project/server.py
+++ final_project/server.py
@@ -1,21 +1,27 @@
 from machinetranslation import translator
 from flask import Flask, render_template, request
 import json
-import machinetranslation
+from machinetranslation.translator import english_to_french, french_to_english
 
 app = Flask("Web Translator")
 
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to French"
+
+  
+    translated_text = english_to_french(textToTranslate)
+    return  f'Translated text in French: {translated_text}'
+
+
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to English"
+
+   
+    translated_text = french_to_english(textToTranslate)
+    return f'Translated text in English: {translated_text}'
 
 @app.route("/")
 def renderIndexPage():
