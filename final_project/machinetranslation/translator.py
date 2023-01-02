f613e71f454c70101c87e7218434128425f41ad7
DerSimulant <82386236+DerSimulant@users.noreply.github.com>
2023-01-02 14:42:57 +0100

    Added unittests

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,7 +1,11 @@
+'''
+This module is a translator utilizing the IBM wastom web service via API
+'''
+
 import json
+import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -14,28 +18,30 @@ lt = LanguageTranslatorV3(
     version='2018-05-01',
     authenticator=authenticator)
 
-lt.set_service_url('url')
+lt.set_service_url(url)
 
 
-
-def englishToFrench(englishText):
+def english_to_french(english_text):
     '''Translates the English text in the string variable "text" to French
-    and returns the translation. To get to the translation you need to access the 
+    and returns the translation. To get to the translation you need to access the
     1st data in the JSON returned. Rest is additional data'''
 
-    frenchText = lt.translate(
-    text=englishText,
+    french_text = lt.translate(
+    text=english_text,
     model_id='en-fr').get_result()
-    print(json.dumps(frenchText, indent=2, ensure_ascii=False))
-    return frenchText
+    print(json.dumps(french_text, indent=2, ensure_ascii=False))
+    return french_text['translations'][0]['translation']
 
-def frenchToEnglish(frenchText):
+def french_to_english(french_text):
     '''Translates the French text in the string variable "text" to English
-    and returns the translation. To get to the translation you need to access the 
+    and returns the translation. To get to the translation you need to access the
     1st data in the JSON returned. Rest is additional data'''
-    
-    englishText = lt.translate(
-    text=frenchText,
-    model_id='en-fr').get_result()
-    print(json.dumps(englishText, indent=2, ensure_ascii=False))
-    return englishText 
+
+    english_text = lt.translate(
+    text=french_text,
+    model_id='fr-en').get_result()
+    print(json.dumps(english_text, indent=2, ensure_ascii=False))
+    return english_text['translations'][0]['translation']
+
+#print(englishToFrench("Hello my name is Sebastian "))
+#print(englishToFrench("Hello my name is Sebastian ")['translations'][0]['translation'])
